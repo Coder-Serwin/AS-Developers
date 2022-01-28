e448c90ff90508b847a938211ec12089d7ca6f9f
Coder-Serwin <93427110+Coder-Serwin@users.noreply.github.com>
2022-01-28 11:46:47 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,40 @@
+from flask import Flask , send_file , render_template ,request
+import time
+
+message = "Subject : From AS Developers \n\nThanks for subscribing!"
+app = Flask(__name__)
+p = ""
+Gamename = ""
+
+@app.route("/")
+def home():
+    return render_template("Index.html")
+
+@app.route("/games/<GameName>")
+def Game(GameName):
+    global Gamename
+    Gamename = GameName
+    check_path(Gamename)
+    return render_template("Game.html",game_name=GameName)
+
+def check_path(Gamename):
+    global p
+    if Gamename.startswith("Catch"):
+        p = "templates/Game.html"
+    elif Gamename == "":
+        p = "main.py"
+    else:
+        p = ".env"
+
+
+@app.route("/download_file")
+def download_file():
+    return render_template("download.html")
+
+
+@app.route("/download")
+def download():
+    return send_file(p,as_attachment=True)
+
+if __name__ == "__main__":
+    app.run(debug=True)
